1ee9425c23c9410e61ad0d51a81becaa9c1812cc
debtmusicorig-jpg <d.e.b.t.music.orig@gmail.com>
2026-06-03 12:14:36 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,4 @@
-
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  logLevel: 'error', // Suppress warnings, only show errors
-  plugins: 
-    react(),
-  ]
-});
+export default defineConfig({ plugins: [react()] })
