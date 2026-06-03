569ab9089d2b2f435481545cf6855c390f3db981
debtmusicorig-jpg <d.e.b.t.music.orig@gmail.com>
2026-06-03 13:33:17 +0100

    Update BioText.jsx

--- src/entities/BioText.jsx
+++ src/entities/BioText.jsx
@@ -1,7 +1,24 @@
-export default function BioText() {
-  return (
-    <p>
-      Welcome to D.E.B.T-Music. Custom singer/songwriter, rock, and folk compositions.
-    </p>
-  )
+{
+  "name": "BioText",
+  "type": "object",
+  "properties": {
+    "title": {
+      "type": "string",
+      "default": "Biography"
+    },
+    "content": {
+      "type": "string"
+    }
+  },
+  "required": [
+    "content"
+  ],
+  "rls": {
+    "read": {},
+    "write": {
+      "user_condition": {
+        "role": "admin"
+      }
+    }
+  }
 }
