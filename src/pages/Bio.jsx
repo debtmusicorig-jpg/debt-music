88d34a4bc7d38a9ccc07d8df41b1e92589d36507
debtmusicorig-jpg <d.e.b.t.music.orig@gmail.com>
2026-06-03 15:29:57 +0100

    Update Bio.jsx

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Layout from '../Layout.jsx'
 import Releases from '../components/Releases.jsx'
+import BioText from '../entities/BioText.json'
+import BioMedia from '../entities/BioMedia.json'
 
 export default function BioPage() {
   return (
     <Layout>
-      <Releases />
+      <Releases textData={BioText} mediaData={BioMedia} isPreview={true} />
     </Layout>
   )
 }
